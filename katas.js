3d86bcd5fb152194b3636f5aa84a9cd726025cc4
SuShruthi <shruthacm@gmail.com>
2023-03-08 11:56:03 -0800

    added kata 2

--- katas.js
+++ katas.js
@@ -8,28 +8,7 @@ const sumLargestNumbers = function (data) {
   // Add the two largest numbers
   return sortedData[0] + sortedData[1];
 };
-console.log("Kata 1");
+//console.log("Kata 1");
 console.log(sumLargestNumbers([1, 10])); // Expected output: 11
 console.log(sumLargestNumbers([1, 2, 3])); // Expected output: 5
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); // Expected output: 126
-
-//Create a function named conditionalSum that will be given an array of numbers and a condition, in this case odd or even. Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
-
-//Warning: Use some sort of looping. Do Not use Array.prototype.filter()
-
-const conditionalSum = function (values, condition) {
-  let sum = 0;
-  for (let i = 0; i < values.length; i++) {
-    if (condition === "even" && values[i] % 2 === 0) {
-      sum += values[i];
-    } else if (condition === "odd" && values[i] % 2 !== 0) {
-      sum += values[i];
-    }
-  }
-  return sum;
-};
-console.log("Kata 2");
-console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
-console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
-console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
-console.log(conditionalSum([], "odd"));
